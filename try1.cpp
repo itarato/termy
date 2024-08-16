63d3101b0234bc968ea0b739e80cfca7bd0e0632
Peter Arato <it.arato@gmail.com>
2024-08-15 22:14:36 -0400

    Ownership.

--- try1.cpp
+++ try1.cpp
@@ -4,14 +4,27 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
+    // Opening the unused master.
     int pty_master_fd = posix_openpt(O_RDWR | O_NOCTTY);
-    if (pty_master_fd == -1) {
+    if (pty_master_fd == -1)
+    {
         printf("Error: cannot create master PTY.\n");
         return 1;
     }
 
     printf("Master PTY has been created, FD: %d.\n", pty_master_fd);
+
+    // Change slave ownership and permission.
+    int grantpt_result = grantpt(pty_master_fd);
+    if (grantpt_result == -1)
+    {
+        printf("Error: failed updating slave ownership and perms.\n");
+        return 2;
+    }
+
+    printf("Slave ownership and perms has been set. \n");
 
     return 0;
 }
