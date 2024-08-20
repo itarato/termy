f56a5f1db49f979165f9cfe8716c20c29db3545a
Peter Arato <it.arato@gmail.com>
2024-08-19 21:32:33 -0400

    Add per process io handling.

--- try1.cpp
+++ try1.cpp
@@ -385,7 +385,7 @@ int main(void) {
   // Parent process.
 
   int script_fd =
-      open("try1_output", O_WRONLY | O_CREAT | O_TRUNC,
+      open("output", O_WRONLY | O_CREAT | O_TRUNC,
            S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
   if (script_fd == -1) {
     perror("Parent | Error: cannot open output file.\n");
